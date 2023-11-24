013b1342845a9a4be401cedcdbd4e543fe26b5e7
pabloivorra <145840713+pabloivorra@users.noreply.github.com>
2023-11-24 20:02:21 +0100

    Database Connection

--- db/db_connection.py
+++ db/db_connection.py
@@ -1,1 +1,31 @@
 # Database connection functions
+import mysql.connector
+from mysql.connector import Error
+
+
+def obtener_conexion():
+    try:
+        conexion = mysql.connector.connect(
+            host='192.168.1.212',
+            user='eloy',
+            password='eloyflexo',
+            database='flexografico'
+        )
+        return conexion
+    except Error as e:
+        print(f"Error al conectar a la base de datos MySQL: {e}")
+        return None
+
+def realizar_consulta(consulta):
+    try:
+        conexion = obtener_conexion()
+        if conexion is not None:
+            cursor = conexion.cursor()
+            cursor.execute(consulta)
+            resultados = cursor.fetchall()
+            return resultados
+    except Error as e:
+        print(f"Error al realizar consulta: {e}")
+    finally:
+        if conexion is not None and conexion.is_connected():
+            conexion.close()
